78373becde3eb96a877bcd80988db68bbf51f951
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-17 17:02:25 -0500

    Add create and new methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+    before_action :authenticate, only: [:show, :edit]
+
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+          flash[:success] = "Post successfully created"
+          redirect_to @post
+        else
+          flash[:error] = "Something went wrong, Please Try Again"
+          render 'new'
+        end
+    end
+    
+    private
+    def post_params
+        params.require(:post).permit(:post)
+    end
+
 end
